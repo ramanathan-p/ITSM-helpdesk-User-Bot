4b6e92a88ec61413fd70a041daa5cd68b8685b10
Ramanathan P <ramanathan.p@congruentindia.com>
2017-12-20 14:50:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+'use strict';
+
+
+console.log('11111111111');
+
+/*
+const apiai = require('apiai');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const SkypeBot = require('./skypebot');
+const SkypeBotConfig = require('./skypebotconfig');
+
+const REST_PORT = (process.env.PORT || 5000);
+
+const botConfig = new SkypeBotConfig(
+    process.env.APIAI_ACCESS_TOKEN,
+    process.env.APIAI_LANG,
+    process.env.APP_ID,
+    process.env.APP_SECRET
+);
+
+const skypeBot = new SkypeBot(botConfig);
+
+
+
+// console timestamps
+require('console-stamp')(console, 'yyyy.mm.dd HH:MM:ss.l');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/chat', skypeBot.botService.listen());
+
+app.listen(REST_PORT, function () {
+    console.log('Rest service ready on port ' + REST_PORT);
+}); */
+
 var restify = require('restify');
 var builder = require('botbuilder');
 var apiairecognizer = require('api-ai-recognizer');
@@ -18,7 +72,7 @@ var connector = new builder.ChatConnector({
 });
 
 // Listen for messages from users 
-server.post('/webhook', connector.listen());
+server.post('/chat', connector.listen());
 //server.post('/chatbot', connector.listen());
 //server.post('/', connector.listen());
 
@@ -574,18 +628,6 @@ intents.onDefault(function(session, args){
 server.listen(process.env.port || process.env.PORT || 7070, function () { //6060
    console.log('%s listening to %s', server.name, server.url); 
  
-        /*
-        server.on('request', function(req, res){
-            res.send('<h1>Hello World :: </h1>');
-            res.end();
-        });*/
-
-        var io = require('socket.io').listen(server.listen(7070));
-        console.log('io: ',io);
-      
-        
-       
-       
-
-
 });
+
+console.log('22222222222');
